26b32385827ef3c5871e3db5179a9240e0b5a401
he_yulin <he_yulin@163.com>
2018-10-10 13:37:11 +0800

    修改配置

---
+++ src/settings/prod.py
@@ -0,0 +1,27 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'NAME': 'welljia',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root123456789',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',        
+      },
+    }
+
+
+#TENCENT = {
+    #'SdkAppId': '1400091635',
+    #'AppKey': '3d5e973b84eb1d76a3b92c05bdbbdc85',
+#}
+
+TENCENT = {
+    'SdkAppId': '1400144729',
+    'AppKey': '1a8093135b4e91df3b7d76535437a98f',
+    'validate_temp': '199823',
+}
+
+ALLOWED_HOSTS = ['www.werjia.cn', 'werjia.cn']
