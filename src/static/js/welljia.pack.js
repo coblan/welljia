3768a242dbf0ac6697af08d67c28579d1cd4afa7
he_yulin <he_yulin@163.com>
2018-10-24 21:01:01 +0800

    debug

--- src/static/js/welljia.pack.js
+++ src/static/js/welljia.pack.js
@@ -406,7 +406,8 @@ Vue.component('com-pop-huxing', {
     props: ['ctx'],
     data: function data() {
         return {
-            crt_view: '2d'
+            crt_view: '2d',
+            read_3d: ''
         };
     },
     computed: {
@@ -414,8 +415,12 @@ Vue.component('com-pop-huxing', {
             return '/3d_wrap?d3_url=' + encodeURIComponent(this.ctx.floor.img_3d);
         }
     },
-
-    template: '<div class="com-pop-huxing"  style="position: absolute;top:0;left: 0;bottom: 0;right: 0;">\n\n             <img v-if="crt_view==\'2d\'" class="center-vh" :src="ctx.floor.img_2d" style="max-width: 95%;max-height:95%" alt="">\n             <iframe allowvr="yes" scrolling="no" v-if="crt_view==\'3d\'" :src="wraped_3d" frameborder="0" width="100%" height="100%"></iframe>\n             <div class="toogle-btn clickable" v-if="crt_view==\'2d\'" @click="crt_view=\'3d\'">3D</div>\n             <div class="toogle-btn clickable" v-if="crt_view==\'3d\'" @click="crt_view=\'2d\'">2D</div>\n    </div>'
+    methods: {
+        start_read: function start_read() {
+            this.read_3d = this.wraped_3d;
+        }
+    },
+    template: '<div class="com-pop-huxing"  style="position: absolute;top:0;left: 0;bottom: 0;right: 0;">\n             <img v-if="crt_view==\'2d\'" class="center-vh" :src="ctx.floor.img_2d" style="max-width: 95%;max-height:95%" alt="">\n             <iframe allowvr="yes" scrolling="no" v-show="crt_view==\'3d\'" :src="wraped_3d" frameborder="0" width="100%" height="100%"></iframe>\n             <!--<iframe  allowvr="yes" scrolling="no" v-if="crt_view==\'3d\'" :src="read_3d" frameborder="0" width="100%" height="100%"></iframe>-->\n\n             <div class="toogle-btn clickable" v-if="crt_view==\'2d\'" @click="crt_view=\'3d\'">3D</div>\n             <div class="toogle-btn clickable" v-if="crt_view==\'3d\'" @click="crt_view=\'2d\'">2D</div>\n    </div>'
 });
 
 /***/ }),
--- src/webpage/js/comp/builder_floor.js
+++ src/webpage/js/comp/builder_floor.js
@@ -39,6 +39,7 @@ Vue.component('com-pop-huxing',{
     data:function(){
         return {
             crt_view:'2d',
+            read_3d:''
         }
     },
     computed:{
@@ -46,11 +47,16 @@ Vue.component('com-pop-huxing',{
             return '/3d_wrap?d3_url='+encodeURIComponent(this.ctx.floor.img_3d)
         }
     },
-
+    methods:{
+        start_read:function(){
+            this.read_3d= this.wraped_3d
+        }
+    },
     template:`<div class="com-pop-huxing"  style="position: absolute;top:0;left: 0;bottom: 0;right: 0;">
-
              <img v-if="crt_view=='2d'" class="center-vh" :src="ctx.floor.img_2d" style="max-width: 95%;max-height:95%" alt="">
-             <iframe allowvr="yes" scrolling="no" v-if="crt_view=='3d'" :src="wraped_3d" frameborder="0" width="100%" height="100%"></iframe>
+             <iframe allowvr="yes" scrolling="no" v-show="crt_view=='3d'" :src="wraped_3d" frameborder="0" width="100%" height="100%"></iframe>
+             <!--<iframe  allowvr="yes" scrolling="no" v-if="crt_view=='3d'" :src="read_3d" frameborder="0" width="100%" height="100%"></iframe>-->
+
              <div class="toogle-btn clickable" v-if="crt_view=='2d'" @click="crt_view='3d'">3D</div>
              <div class="toogle-btn clickable" v-if="crt_view=='3d'" @click="crt_view='2d'">2D</div>
     </div>`
