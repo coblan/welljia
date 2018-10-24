44bb5533990dc37cecb1d8396b5cddd42aaa1ad1
he_yulin <he_yulin@163.com>
2018-10-24 16:34:49 +0800

    debug

--- src/webpage/static/js/welljia.pack.js
+++ src/webpage/static/js/welljia.pack.js
@@ -409,7 +409,7 @@ Vue.component('com-pop-huxing', {
             crt_view: '2d'
         };
     },
-    template: '<div class="com-pop-huxing"  style="position: absolute;top:0;left: 0;bottom: 0;right: 0;">\n\n             <img v-if="crt_view==\'2d\'" class="center-vh" :src="ctx.floor.img_2d" style="max-width: 95%;max-height:95%" alt="">\n             <iframe v-if="crt_view==\'3d\'" :src="ctx.floor.img_3d" frameborder="0" width="100%" height="100%"></iframe>\n             <div class="toogle-btn clickable" v-if="crt_view==\'2d\'" @click="crt_view=\'3d\'">3D</div>\n             <div class="toogle-btn clickable" v-if="crt_view==\'3d\'" @click="crt_view=\'2d\'">2D</div>\n    </div>'
+    template: '<div class="com-pop-huxing"  style="position: absolute;top:0;left: 0;bottom: 0;right: 0;">\n\n             <img v-if="crt_view==\'2d\'" class="center-vh" :src="ctx.floor.img_2d" style="max-width: 95%;max-height:95%" alt="">\n             <iframe allowvr="yes" scrolling="no" v-if="crt_view==\'3d\'" :src="ctx.floor.img_3d" frameborder="0" width="100%" height="100%"></iframe>\n             <div class="toogle-btn clickable" v-if="crt_view==\'2d\'" @click="crt_view=\'3d\'">3D</div>\n             <div class="toogle-btn clickable" v-if="crt_view==\'3d\'" @click="crt_view=\'2d\'">2D</div>\n    </div>'
 });
 
 /***/ }),
