ff8cff479468dfcb044bb954583ffb071302cbcf
he_yulin <he_yulin@163.com>
2018-12-19 11:30:09 +0800

    debug

--- src/webpage/models.py
+++ src/webpage/models.py
@@ -50,7 +50,7 @@ class MapPoint(models.Model):
     """一个建筑群"""
     title = models.CharField('显示名', max_length = 100)
     pos = models.CharField('坐标', max_length = 30, help_text = '请严格按照 x,y 的格式填写')
-    url = models.CharField('3D资源地址', max_length = 200)
+    url = models.CharField('3D资源地址', max_length = 200,blank=True,)
     
     def __str__(self): 
         return self.title
